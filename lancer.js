96a2f0d3a73caaf444089d9c0eb13c2bebeee918
Mapelujo <olamideiyanda18@gmail.com>
2023-09-16 15:54:34 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -30,25 +30,24 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//////////// LECTURES ///////////////////////
+
 // SELECTING ELEMENTS //
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
 const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section-1');
-const allButtons = document.getElementsByTagName('button'); // This returns an HTML Collection
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
+// document.getElementById('section-1');
+// const allButtons = document.getElementsByTagName('button'); // This returns an HTML Collection
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
 
 // CREATING AND INSERTING ELEMENTS (PROGRAMMATICALY)//
-
 // .insertAdjacentHTML
-
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = 'We use cookies for improved functionality and analytics';
@@ -66,5 +65,21 @@ header.append(message);
 // DELETING ELEMENTS //
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove(); // The much newer method of deleting elements
-    // message.parentElement.removeChild(message); // The previous way of deleting elements
+  // message.parentElement.removeChild(message); // The previous way of deleting elements
 });
+
+//// STYLES, ATTRIBUTES AND CLASSES ////
+
+// STYLES //
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');  // changing the value of custom css properties.
+
+
