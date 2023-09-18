1be8cc57a52ec48472a56dc08ae4c8a3d5a0bba0
Mapelujo <olamideiyanda18@gmail.com>
2023-09-18 22:33:25 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -10,7 +10,7 @@ const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('nav');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');  
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////// FUNCTIONS /////////////////////////////
 
@@ -26,6 +26,21 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+//////////////// APPLICATIONS //////////////////
+
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
@@ -39,61 +54,12 @@ document.addEventListener('keydown', function (e) {
 
 // BUTTON SCROLLING //
 btnScrollTo.addEventListener('click', e => {
-  //   const s1coords = section1.getBoundingClientRect();
-  //   console.log(s1coords);
-  //   console.log(e.target.getBoundingClientRect());
-  //   console.log(
-  //     'Current Scroll (X/Y) position:',
-  //     window.pageXOffset,
-  //     pageYOffset
-  //   );
-  //   console.log(
-  //     'Height/Width Viewport',
-  //     document.documentElement.clientHeight,
-  //     document.documentElement.clientWidth
-  //   );
-
-  // Scrolling
-  //   window.scrollTo(
-  //     s1coords.left + window.pageXOffset, // Calculating the absolute position of the element relative to the document i.e the entire page.
-  //     s1coords.top + window.pageYOffset   // Calculating the absolute position of the element relative to the document i.e the entire page.
-  //   );
-
-  // Old Method of implementing the scroll functionality
-  //   window.scrollTo({
-  //     left: s1coords.left + window.pageXOffset, // Calculating the absolute position of the element relative to the document i.e the entire page.
-  //     top: s1coords.top + window.pageYOffset, // Calculating the absolute position of the element relative to the document i.e the entire page.
-  //     behavior: 'smooth',
-  //   });
-
-  // Newer method of implementing it.
   section1.scrollIntoView({
     behavior: 'smooth',
   });
 });
 
 // PAGE NAVIGATION
-
-// WITHOUT EVENT DELEGATION
-// document.querySelectorAll('.nav__link').forEach((link, i) => {
-//   link.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     console.log(`LINK-${i + 1}`);
-
-//     // Implementing the smooth scrolling
-//     const id = this.getAttribute('href');
-//     console.log(id);
-
-//     document.querySelector(id).scrollIntoView({
-//       behavior: 'smooth',
-//     });
-//   });
-// });
-
-// WITH EVENT DELEGATION
-// - Add the event listener to the common parent element
-// - Determine what element originated the event
-
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -129,30 +95,11 @@ tabsContainer.addEventListener('click', e => {
     .classList.add('operations__content--active');
 });
 
-
 //   MENU FADE ANIMATION
-nav.addEventListener('mouseover',function(e){
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
-
-    siblings.forEach((el => {
-      if (el !== link) el.style.opacity = 0.5;
-    }))
-    logo.style.opacity = 0.5;
-  }
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
 });
 
-nav.addEventListener('mouseout',function(e){
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
-
-    siblings.forEach((el => {
-      if (el !== link) el.style.opacity = 1;
-    }))
-    logo.style.opacity = 1;
-  }
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
 });
