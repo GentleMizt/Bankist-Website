abb4a2a113078bc529185c70727efd5ebb23d376
Mapelujo <olamideiyanda18@gmail.com>
2023-09-18 15:08:48 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -101,3 +101,23 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     });
   }
 });
+
+
+/// DOM TRAVERSING ///
+
+const h1 = document.querySelector('h1');
+
+// going down the DOM: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes); 
+console.log(h1.children); // Works only for direct children
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// going up the DOM: Parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
