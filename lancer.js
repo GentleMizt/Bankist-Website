93200185390078f450b6258c9cc0e30106f76cab
Mapelujo <olamideiyanda18@gmail.com>
2023-09-18 22:46:17 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -26,17 +26,16 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-const handleHover = function(e, opacity){
-  console.log(this);
+const handleHover = function(e){
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach((el => {
-      if (el !== link) el.style.opacity = opacity;
+      if (el !== link) el.style.opacity = this;
     }))
-    logo.style.opacity = opacity;
+    logo.style.opacity = this;
   }
 }
 
