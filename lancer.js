1369df393bce0e95f51cf21fed7c69c1fe715215
Mapelujo <olamideiyanda18@gmail.com>
2023-09-18 22:27:03 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -7,6 +7,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');  
 
 ///////////////////////////// FUNCTIONS /////////////////////////////
 
@@ -103,10 +107,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', e => {
   e.preventDefault();
 
@@ -127,4 +127,24 @@ tabsContainer.addEventListener('click', e => {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
+});
+
+
+//   MENU FADE ANIMATION
+nav.addEventListener('mouseover',function(e){
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    console.log(siblings);
+    siblings.forEach((el => {
+      if (el !== link) el.style.opacity = 0.5;
+    }))
+    logo.style.opacity = 0.5;
+  }
+});
+
+nav.addEventListener('mouseout',function(e){
+
 });
