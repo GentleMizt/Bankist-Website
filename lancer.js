8fb9c1e5aa802edf278d74720051d9aa3e8c56e3
Mapelujo <olamideiyanda18@gmail.com>
2023-09-18 21:19:08 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -112,20 +112,18 @@ tabsContainer.addEventListener('click', e => {
 
   // Getting the Matching strategy
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
 
   // Guard Clause
   if (!clicked) return;
 
   // removing active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach((c => c.classList.remove('operations__content--active')));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
   // Active tab
   clicked.classList.add('operations__tab--active');
 
   // Activating Content Area/Information
-  console.log(clicked.dataset.tab);
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
